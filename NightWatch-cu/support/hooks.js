a2eac155f398230dcd1d8a51b9c623d119c68b42
vilcica <48692206+vilcica@users.noreply.github.com>
2019-08-14 12:25:56 +0300

    Update hooks.js

---
+++ NightWatch-cu/support/hooks.js
@@ -0,0 +1,17 @@
+const { Before, After, setDefaultTimeout } = require("cucumber");
+const { client } = require("nightwatch-cucumber");
+
+setDefaultTimeout(30 * 1000);
+
+Before(async () => {
+  await client.session(async () => {
+    console.log('START TESTING!');
+  });
+});
+
+// Code executed after each scenario.
+After(async scenario => {
+  if (scenario.result.status === "failed") {
+    console.log(`Scenario '${scenario.pickle.name}' is failed`);
+  }
+});
